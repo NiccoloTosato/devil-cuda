7e9b00574be9d47dbe13355e390bc5bee7e844a4
NiccoloTosato <niccolo.tosato@yahoo.it>
2024-07-29 14:11:09 +0200

    Finally something smart!

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include<cuda.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <iostream>
+#include <ostream>
+
+#include "utils.hpp"
+
+int main() {
+	std::cout<< "This is my main file" << std::endl;
+
+}
+
+
